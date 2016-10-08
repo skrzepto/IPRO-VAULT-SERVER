98c2eb091d85554148aaece7cfca61746052aa35
Szymon <shims506@gmail.com>
2016-10-08 13:43:50 -0500

    moved models contents into handlers, gofmt the code, error handling if the sensor_id is not found in the map

--- app.go
+++ app.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-    "net/http"
-    "io"
-    "log"
-    "github.com/julienschmidt/httprouter"
-    "github.com/skrzepto/IPRO-VAULT-SERVER/handlers"
+	"github.com/julienschmidt/httprouter"
+	"github.com/skrzepto/IPRO-VAULT-SERVER/handlers"
+	"io"
+	"log"
+	"net/http"
 )
 
 func hello(rw http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-    io.WriteString(rw, "Hello world!")
+	io.WriteString(rw, "Hello world!")
 }
 
 func main() {
-    i := handlers.InitGlobal()
-    router := httprouter.New()
+	i := handlers.InitGlobal()
+	router := httprouter.New()
 
-    router.GET("/", hello)
-    router.GET("/api/sensor_data", i.SensorData)
-    router.POST("/api/sensor_data", i.SensorData)
-    router.GET("/api/sensor_data/:sensor_id", i.GET_SensorData_ID)
-    router.POST("/api/sensor_data/:sensor_id", i.POST_SensorData_ID)
+	router.GET("/", hello)
+	router.GET("/api/sensor_data", i.SensorData)
+	router.POST("/api/sensor_data", i.SensorData)
+	router.GET("/api/sensor_data/:sensor_id", i.GET_SensorData_ID)
+	router.POST("/api/sensor_data/:sensor_id", i.POST_SensorData_ID)
 
-    log.Fatal(http.ListenAndServe(":8082", router))
+	log.Fatal(http.ListenAndServe(":8082", router))
 }
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,65 +1,80 @@
 package handlers
 
 import (
-    "net/http"
-    "encoding/json"
-    "fmt"
-    "sync"
-    "github.com/julienschmidt/httprouter"
-    "github.com/skrzepto/IPRO-VAULT-SERVER/models"
+	"encoding/json"
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"sync"
+	"time"
 )
 
+type SensorData struct {
+	Name          string    `json:"name"`
+	Location      string    `json:"location"`
+	Serial_Number int       `json:"serial_number"`
+	Temperate     float64   `json:"temperature"`
+	Pressure      float64   `json:"pressure"`
+	Humidity      float64   `json:"humidity"`
+	Water_Level   float64   `json:"water_level"`
+	CreatedAt     time.Time `json:"createdAt"`
+	UpdatedAt     time.Time `json:"updatedAt"`
+}
+
 type Impl struct {
-  sd_map map[string]models.SensorData
-  mu sync.Mutex
+	sd_table map[string]SensorData
+	mu       sync.Mutex
 }
 
 func InitGlobal() *Impl {
-    var i Impl
-    i.sd_map = make(map[string]models.SensorData)
-    return &i
+	var i Impl
+	i.sd_table = make(map[string]SensorData)
+	return &i
 }
 
-
 func (i *Impl) POST_SensorData_ID(rw http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-    decoder := json.NewDecoder(req.Body)
-    var sd models.SensorData
-    err := decoder.Decode(&sd)
-    if err != nil {
-        panic(err)
-    }
-    fmt.Printf("decoded to %#v\n", sd)
-    //i.DB.Create(&t) change this to array
-    sensor_id := ps.ByName("sensor_id")
-    i.mu.Lock()
-    i.sd_map[sensor_id] = sd
-    i.mu.Unlock()
+	decoder := json.NewDecoder(req.Body)
+	var sd SensorData
+	err := decoder.Decode(&sd)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("decoded to %#v\n", sd)
+	//i.DB.Create(&t) change this to array
+	sensor_id := ps.ByName("sensor_id")
+	i.mu.Lock()
+	i.sd_table[sensor_id] = sd
+	i.mu.Unlock()
 }
 
 func (i *Impl) GET_SensorData_ID(rw http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-    sensor_id := ps.ByName("sensor_id")
-    i.mu.Lock()
-    sd := i.sd_map[sensor_id]
-    i.mu.Unlock()
-
-    js, err := json.Marshal(sd)
-    if err != nil {
-      http.Error(rw, err.Error(), http.StatusInternalServerError)
-      panic(err)
-    }
-    rw.Header().Set("Content-Type", "application/json")
-    rw.Write(js)
+	sensor_id := ps.ByName("sensor_id")
+	i.mu.Lock()
+	sd, exist:= i.sd_table[sensor_id]
+	i.mu.Unlock()
+  if !exist {
+    rw.WriteHeader(404)
+    rw.Write([]byte("Can't find sendor data for the id specified"))
+    return
+  }
+	js, err := json.Marshal(sd)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		panic(err)
+	}
+	rw.Header().Set("Content-Type", "application/json")
+	rw.Write(js)
 }
 
 // [GET] /api/sensor_data
 func (i *Impl) SensorData(rw http.ResponseWriter, req *http.Request, _ httprouter.Params) {
-    if req.Method == "GET" {
-      js, err := json.Marshal(i.sd_map)
-      if err != nil {
-        http.Error(rw, err.Error(), http.StatusInternalServerError)
-        panic(err)
-      }
-      rw.Header().Set("Content-Type", "application/json")
-      rw.Write(js)
-    }
+	if req.Method == "GET" {
+		js, err := json.Marshal(i.sd_table)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			panic(err)
+		}
+		rw.Header().Set("Content-Type", "application/json")
+		rw.Write(js)
+	}
 }
